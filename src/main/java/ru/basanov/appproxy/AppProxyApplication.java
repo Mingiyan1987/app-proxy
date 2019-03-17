d028f70be573c99f0e7ef38bccbdea2208b58ace
Mingiyan1987 <basanov.mt87@gmail.com>
2019-03-17 13:36:16 +0300

    first-commit

---
+++ src/main/java/ru/basanov/appproxy/AppProxyApplication.java
@@ -0,0 +1,13 @@
+package ru.basanov.appproxy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppProxyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AppProxyApplication.class, args);
+    }
+
+}
